366e437941bcb3bff0e22e857dedc34d728d1353
Rainer <rainer.blessing@gmail.com>
2019-05-19 16:50:48 +0200

    Scoring + Bugfix

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,10 @@ use sdl2::TimerSubsystem;
 
 use std::time::Duration;
 use sdl2::rect::Rect;
-use std::convert::TryFrom;
 
 use rand::Rng;
+use std::path::Path;
+use sdl2::render::TextureQuery;
 
 const WIDTH: u32 = 800;
 const HEIGHT: u32 = 800;
@@ -32,16 +33,56 @@ struct PlayField {
     y: u32,
 }
 
+// handle the annoying Rect i32
+macro_rules! rect(
+    ($x:expr, $y:expr, $w:expr, $h:expr) => (
+        Rect::new($x as i32, $y as i32, $w as u32, $h as u32)
+    )
+);
+
+// Scale fonts to a reasonable size when they're too big (though they might look less smooth)
+fn get_centered_rect(rect_width: u32, rect_height: u32, cons_width: u32, cons_height: u32) -> Rect {
+    let wr = rect_width as f32 / cons_width as f32;
+    let hr = rect_height as f32 / cons_height as f32;
+
+    let (w, h) = if wr > 1f32 || hr > 1f32 {
+        if wr > hr {
+            println!("Scaling down! The text will look worse!");
+            let h = (rect_height as f32 / wr) as i32;
+            (cons_width as i32, h)
+        } else {
+            println!("Scaling down! The text will look worse!");
+            let w = (rect_width as f32 / hr) as i32;
+            (w, cons_height as i32)
+        }
+    } else {
+        (rect_width as i32, rect_height as i32)
+    };
+
+    let cx = (WIDTH as i32 - w) / 2;
+    let cy = (HEIGHT as i32 - h) / 2;
+    rect!(cx, cy, w, h)
+}
+
 pub fn main() {
+    let font_path: &Path = Path::new("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf");
+
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
 
-    let window = video_subsystem.window("SDL2 Snake", WIDTH+200, HEIGHT)
+    let ttf_context = sdl2::ttf::init().unwrap();;
+
+    let mut font = ttf_context.load_font(font_path, 128).unwrap();;
+    font.set_style(sdl2::ttf::FontStyle::BOLD);
+
+    let window = video_subsystem.window("SDL2 Snake", WIDTH, HEIGHT)
         .position_centered()
         .build()
         .unwrap();
 
     let mut canvas = window.into_canvas().build().unwrap();
+    let texture_creator = canvas.texture_creator();
+
 
     let mut event_pump = sdl_context.event_pump().unwrap();
 
@@ -61,12 +102,11 @@ pub fn main() {
     const M: usize = 40;
     const N: usize = 40;
 
+    let mut score: i32 = 0;
+
     #[derive(Copy, Clone)]
     let mut snake_vec= Vec::new();
     snake_vec.push(PlayField{x:20,y:20,field_type:Type::Snake});
-    snake_vec.push(PlayField{x:20,y:21,field_type:Type::Snake});
-    snake_vec.push(PlayField{x:20,y:22,field_type:Type::Snake});
-    snake_vec.push(PlayField{x:20,y:23,field_type:Type::Snake});
 
     #[derive(Copy, Clone)]
     let mut apple_vec= Vec::new();
@@ -97,9 +137,9 @@ pub fn main() {
             }
         }
 
-        println!("-------------------");
+//        println!("-------------------");
         for snake_elem in &snake_vec{
-            println!("y {} x {} x1 {} y1 {}", y, x, snake_elem.x,snake_elem.y);
+//            println!("y {} x {} x1 {} y1 {}", y, x, snake_elem.x,snake_elem.y);
             grid[snake_elem.x as usize][snake_elem.y as usize].field_type = snake_elem.field_type;
         }
 
@@ -107,7 +147,7 @@ pub fn main() {
             grid[apple_elem.x as usize][apple_elem.y as usize].field_type = apple_elem.field_type;
         }
 
-        println!("-------------------");
+//        println!("-------------------");
         canvas.set_draw_color(Color::RGB(0, 0, 0));
         canvas.clear();
         let rect: Rect = Rect::new(x as i32, y as i32, SNAKE_WIDTH, SNAKE_WIDTH);
@@ -177,11 +217,43 @@ pub fn main() {
         snake_vec[0]=snake_elem;
 
         if snake_vec[0].x == apple_vec[0].x && snake_vec[0].y == apple_vec[0].y {
-            println!("HHHHHHHHHHHHHHHHIIIIIIIIIIIIIIIIIIIIIITTTTTTTTTTTTTTTTTT");
             apple_vec[0].x=rng.gen_range(1, SNAKE_WIDTH);
             apple_vec[0].y=rng.gen_range(1, SNAKE_WIDTH);
+            score=score+1;
+
+            match velocity_x{
+                1 => {
+                    snake_vec.push(PlayField{x:snake_vec.last().unwrap().x-1,y:snake_vec.last().unwrap().y,field_type:Type::Snake});
+                }
+                -1 => {
+                    snake_vec.push(PlayField{x:snake_vec.last().unwrap().x+1,y:snake_vec.last().unwrap().y,field_type:Type::Snake});
+                }
+                _ => {}
+            }
+            match velocity_y{
+                1 => {
+                    snake_vec.push(PlayField{y:snake_vec.last().unwrap().x-1,x:snake_vec.last().unwrap().y,field_type:Type::Snake});
+                }
+                -1 => {
+                    snake_vec.push(PlayField{y:snake_vec.last().unwrap().x+1,x:snake_vec.last().unwrap().y,field_type:Type::Snake});
+                }
+                _ => {}
+            }
         }
-        println!("-------------------");
+//        println!("-------------------");
+
+        // render a surface, and convert it to a texture bound to the canvas
+        let surface = font.render(score.to_string().as_str())
+            .blended(Color::RGBA(255, 0, 0, 255)).unwrap();
+        let texture = texture_creator.create_texture_from_surface(&surface).unwrap();
+
+
+        let TextureQuery { width, height, .. } = texture.query();
+// If the example text is too big for the screen, downscale it (and center irregardless)
+        let padding = 64;
+        let target = get_centered_rect(width, height, WIDTH - padding, HEIGHT - padding);
+
+        canvas.copy(&texture, None, Some(target)).unwrap();
 
         canvas.present();
 
@@ -193,7 +265,7 @@ fn loop_delay(frame_delay: i32, timer: &mut TimerSubsystem, ticks: i32) -> () {
     let frame_time = timer.ticks() as i32;
     let frame_time = frame_time - ticks;
     if frame_delay > frame_time {
-        let sleeptime = (frame_delay + 250 - frame_time) as u64;
+        let sleeptime = (frame_delay + 200 - frame_time) as u64;
 
         std::thread::sleep(Duration::from_millis(sleeptime));
     }
@@ -201,11 +273,11 @@ fn loop_delay(frame_delay: i32, timer: &mut TimerSubsystem, ticks: i32) -> () {
 
 fn move_snake(velocity: &mut i32, coordinate: usize, wrap: &mut bool) -> usize {
     let mut coordinate_new:usize = coordinate;
-    if !*wrap && coordinate == 1
+    if !*wrap && coordinate == 1 && *velocity!=0 as i32
     {
-        coordinate_new = 39;
+        coordinate_new = 38;
         *wrap = true;
-    } else if !*wrap && coordinate == 39 {
+    } else if !*wrap && coordinate == 38 && *velocity!=0 as i32{
         coordinate_new = 1;
         *wrap = true;
     } else {
